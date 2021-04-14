b449b791c4829e36889b1dc92f8895b32ec72eae
Cameron Lucas <cameronlucasaim@gmail.com>
2021-04-13 18:06:51 -0600

    init server setup/config

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const db = mongoose.connection;
+
+mongoose.connect(
+  process.env.MONGODB_URL,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  (error) => (error ? console.log("Connection Error: 01: ", error) : null)
+);
+
+db.on("open", () => console.log("Connected to mongodb"));
+db.on("close", () => console.log("Connection to mongodb closed"));
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(port, () => console.log(`Stocky running on port: ${port}`));
