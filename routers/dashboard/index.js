e97b56b87eb2ac30388e8507df1ad7573549f2a7
Cameron Lucas <cameronlucasaim@gmail.com>
2021-04-14 22:17:54 -0600

    adds new view which contain both the new category form and the new inventory item form.

--- routers/dashboard/index.js
+++ routers/dashboard/index.js
@@ -2,4 +2,6 @@ const router = require("express").Router();
 
 router.get("/", (req, res) => res.render("dashboard/index"));
 
+router.get("/new", (req, res) => res.render("dashboard/new"));
+
 module.exports = router;
