f70cee7f5c835c91e90b358b4119b6eb3544dffe
Cameron Lucas <cameronlucasaim@gmail.com>
2021-04-17 11:58:58 -0600

    adds Index, New, Create routes for the dashboard router

--- routers/dashboard/index.js
+++ routers/dashboard/index.js
@@ -1,7 +1,55 @@
 const router = require("express").Router();
+const Category = require("../../models/Category");
+const Inventory = require("../../models/Inventory");
 
-router.get("/", (req, res) => res.render("dashboard/index"));
+router.get("/", async (req, res) => {
+  try {
+    const inventory = await Inventory.find().populate("category");
 
-router.get("/new", (req, res) => res.render("dashboard/new"));
+    res.render("dashboard/index", { inventory });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/new", async (req, res) => {
+  const categories = await Category.find({});
+  res.render("dashboard/new", { categories });
+});
+
+router.post("/category", async (req, res) => {
+  try {
+    const { title } = req.body;
+    await new Category({ title }).save();
+
+    res.redirect("/dashboard/new");
+  } catch (error) {}
+});
+
+router.post("/inventory", async (req, res) => {
+  try {
+    if (req.body.cost > 0) req.body.cost = req.body.cost * 100;
+    if (req.body.price > 0) req.body.price = req.body.price * 100;
+    let inventory = await Inventory.findOne({ sku: req.body.sku });
+    const category = await Category.findById(req.body.category);
+
+    if (inventory) {
+      inventory.store.push(req.session.store._id);
+      await inventory.save();
+    } else {
+      inventory = await new Inventory({
+        store: req.session.store._id,
+        ...req.body,
+      }).save();
+    }
+
+    category.products.push(inventory._id);
+    await category.save();
+
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
